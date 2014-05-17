1499bc690a325d842acaaab777197685ccb90bb6
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-05-17 15:41:22 +0200

    also build a tarball for master

--- mono-opt-nightly/setup.sh
+++ mono-opt-nightly/setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-branch=mono-3.4.0-branch
 
-yum install -y automake autoconf libtool tar which gcc-c++ gettext mono-opt bzip2
-wget https://github.com/mono/mono/archive/$branch.tar.gz
-tar xzf $branch.tar.gz
-cd mono-$branch
-./autogen.sh
-. /opt/mono/env.sh
-make dist
+function buildTarBall {
+  branch=$1
+  yum install -y automake autoconf libtool tar which gcc-c++ gettext mono-opt bzip2
+  wget https://github.com/mono/mono/archive/$branch.tar.gz
+  tar xzf $branch.tar.gz
+  cd mono-$branch
+  ./autogen.sh
+  . /opt/mono/env.sh
+  make dist
+}
+
+buildTarBall master
+buildTarBall mono-3.4.0-branch
 
 # tell the LBS that the calling python script can continue
 echo "LBSScriptFinished"
