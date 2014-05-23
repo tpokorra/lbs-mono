0d2225276eee8f5d050a16ee0a26a453c8886112
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-05-23 07:30:13 +0200

    add build file for monodevelop, including tarball

---
+++ monodevelop-opt/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function buildTarBallFromTag {
+  tag=$1
+  version=$2
+  fileversion=$3
+  git clone https://github.com/mono/monodevelop.git $branch
+  cd $branch
+  git branch release $tag
+  . /opt/mono/env.sh
+  ./autogen.sh
+  make dist
+  cd ..
+  # adjust the spec file for correct version number
+  sed -i "s/%define version.*/%define version $version/g" monodevelop-opt*.spec
+  sed -i "s/%define fileversion.*/%define fileversion $fileversion/g" monodevelop-opt*.spec
+  cp $branch/monodevelop-$version.tar.bz2 ~/tarball
+  mv $branch/monodevelop-$version.tar.bz2 ~/sources
+
+  echo "DONE with building the tarball for " $branch
+}
+
+mkdir ~/sources
+yum install -y git-core automake autoconf libtool tar which gcc-c++ gettext mono-opt bzip2
+
+buildTarBallFromTag monodevelop-4.2.5.0 4.2.5 4.2.5.0
+
+# tell the LBS that the calling python script can continue
+echo "LBSScriptFinished"
