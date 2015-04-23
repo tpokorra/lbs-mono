ca89087430cb0f8dc2f1724567f3c82eea4c21bf
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-04-23 11:35:37 +0200

    monodevelop tarball: install mono-opt from OBS drop old patches

--- monodevelop-tarball/setup.sh
+++ monodevelop-tarball/setup.sh
@@ -14,22 +14,7 @@ function buildTarBallFromTag {
 
   # install the certificates for nuget, see http://stackoverflow.com/questions/15181888/nuget-on-linux-error-getting-response-stream
   # this is needed for Microsoft AspNet
-  # only apply for Monodevelop >= 5.2
-  if [ -f main/src/addins/AspNet/MonoDevelop.AspNet.csproj ]
-  then
-    if [[ $branch == monodevelop-5.2* || $branch == monodevelop-5.3* ]]
-    then
-      patch -p1 < ../nuget_aspnet_5.2.patch || exit 1
-    else
-      if [[ $branch == monodevelop-5.4* ]]
-      then
-        patch -p1 < ../nuget_aspnet.patch || exit 1
-        patch -p1 < ../NUnitRunner.patch || exit 1
-      else
-        patch -p1 < ../nuget_aspnet_master.patch || exit 1
-      fi
-    fi
-  fi
+  patch -p1 < ../nuget_aspnet_master.patch || exit 1
 
   # quick fix to include dlls and .exe files in the tarball
   # reverting commit https://github.com/mono/monodevelop/commit/a6ce3fd8982770e8d72bfdfb1cd8c5d2c11fdd6b
@@ -52,19 +37,16 @@ function buildTarBallFromTag {
 }
 
 mkdir ~/sources
-if [ -f /etc/redhat-release ] 
-then
-  yum install -y git-core make automake autoconf libtool tar which gcc-c++ gettext bzip2
-  # TODO should get these packages from the spec file, BuildRequires:
-  yum install -y automake autoconf libtool mono-opt mono-opt-devel libgdiplus pkgconfig shared-mime-info intltool gtk-sharp2-opt gnome-sharp2-opt
-else
-  apt-get install -y --force-yes git-core automake autoconf libtool tar build-essential gettext mono-opt bzip2
-  # TODO should be done via .dsc file, build required:
-  apt-get install -y --force-yes debhelper automake make libgdiplus bash pkg-config shared-mime-info intltool gtk-sharp2-opt gnome-sharp2-opt autoconf hostname
-fi
 
-buildTarBallFromTag monodevelop-5.9.0.431 5.9 5.9.0.431
+cd /etc/yum.repos.d/
+wget http://download.opensuse.org/repositories/home:tpokorra:mono/Fedora_21/home:tpokorra:mono.repo
+cd -
+
+yum install -y git-core make automake autoconf libtool tar which gcc-c++ gettext bzip2
+yum install -y automake autoconf libtool mono-opt mono-opt-devel libgdiplus pkgconfig shared-mime-info intltool gtk-sharp2-opt gnome-sharp2-opt
+
 #buildTarBallFromTag monodevelop-5.6.3.3 5.6.3 5.6.3.3
+buildTarBallFromTag monodevelop-5.9.0.431 5.9 5.9.0.431
 
 # tell the LBS that the calling python script can continue
 echo "LBSScriptFinished"
