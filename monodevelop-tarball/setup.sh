b72bb70e1f85a291b5433d20f4a36932641fbb24
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2016-06-14 17:18:11 +0200

    monodevelop tarball: install with apt-get

--- monodevelop-tarball/setup.sh
+++ monodevelop-tarball/setup.sh
@@ -53,7 +53,7 @@ else
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
   apt-get update
-  apt-get install git cmake automake autoconf tar intltool \
+  apt-get -y install git cmake automake autoconf tar intltool \
           mono-complete mono-devel fsharp gnome-sharp2 gtk-sharp2 libssh2-1-dev
 fi
 
