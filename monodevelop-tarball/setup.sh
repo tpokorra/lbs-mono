d24e6e89e2a76a3eb56f0c02598227784afaf758
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2016-06-14 16:50:14 +0200

    monodevelop tarball: fix the required packages on Ubuntu

--- monodevelop-tarball/setup.sh
+++ monodevelop-tarball/setup.sh
@@ -49,8 +49,8 @@ else
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
   apt-get update
-  apt-get install mono-complete mono-devel cmake git tar which gtk-sharp2-dev gnome-sharp-dev fsharp monodoc-devel \
-               libssh2-dev
+  apt-get install git cmake automake autoconf tar intltool \
+          mono-complete mono-devel fsharp gnome-sharp2 gtk-sharp2 libssh2-1-dev
 fi
 
 #buildTarBallFromTag monodevelop-5.6.3.3 5.6.3 5.6.3.3
