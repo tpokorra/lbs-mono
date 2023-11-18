524632f65a8f15cb0fb58b7e4546ba2f8433e15a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2023-11-18 04:47:51 +0100

    also build mono binary tarball for Debian Bookworm

--- mono2bintarball/setup.sh
+++ mono2bintarball/setup.sh
@@ -4,15 +4,24 @@ export MONO_PACKAGES="mono-xsp4 mono-fastcgi-server4 ca-certificates-mono"
 
 apt-get -y install python3 python3-distro
 
+source /etc/os-release
+
 # get the installed packages on the target system (at Hostsharing)
-. ~/.ssh/LOGIN_TARGET_SYSTEM_ENV.sh
-ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa_cronjob $TARGET_USER@$TARGET_HOST "dpkg-query -f '\${Package}\n' -W" > pkgs_target.txt || exit -1
+if [[ "$VERSION_CODENAME" == "buster" ]]; then
+  . ~/.ssh/LOGIN_TARGET_SYSTEM_ENV_BUSTER.sh
+elif [[ "$VERSION_CODENAME" == "bookworm" ]]; then
+  . ~/.ssh/LOGIN_TARGET_SYSTEM_ENV_BOOKWORM.sh
+fi
+
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa_cronjob ${TARGET_USER}@${TARGET_HOST} "dpkg-query -f '\${Package}\n' -W" > pkgs_target.txt || exit -1
 
 # we are using our own repo for newer Mono for Debian Buster
-echo "deb [arch=amd64] https://lbs.solidcharity.com/repos/tpokorra/mono/debian/buster buster main" > /etc/apt/sources.list.d/mono-tpokorra.list
-apt-get update
+if [[ "$VERSION_CODENAME" == "buster" ]]; then
+  echo "deb [arch=amd64] https://lbs.solidcharity.com/repos/tpokorra/mono/debian/buster buster main" > /etc/apt/sources.list.d/mono-tpokorra.list
+  apt-get update
+fi
 
-# get the packages that will be installed on a clean Debian Buster system
+# get the packages that will be installed on a clean Debian system
 apt-get install $MONO_PACKAGES --dry-run | grep -E "^Inst " | awk '{print $2}' > pkgs_mono.txt || exit -1
 
 # install the mono packages
