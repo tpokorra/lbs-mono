5a66d846ec774230c39b204550786583c117724b
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2024-04-20 20:09:01 +0200

    moving mono2bintarball to lbs-hostsharing-binaries repo

--- mono2bintarball/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export MONO_PATH=$SCRIPT_PATH/usr/
-export MONO_GAC_PREFIX=$SCRIPT_PATH/usr/
-export PATH=$SCRIPT_PATH/usr/bin:$PATH
